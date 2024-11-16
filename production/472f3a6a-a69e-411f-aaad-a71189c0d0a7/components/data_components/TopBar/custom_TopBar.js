a79ec562f7ef220b293be4191cf72efc37c154e3
JohnyTs2021 <it2022106@hua.gr>
2024-11-16 15:26:25 +0200

    Changes in styles & topBar built

---
+++ production/472f3a6a-a69e-411f-aaad-a71189c0d0a7/components/data_components/TopBar/custom_TopBar.js
@@ -0,0 +1,68 @@
+const topbarcustom_topbar = {
+    props: {
+        model: Object
+    },
+    data() {
+        return {
+            fragments: [],
+            currentIndex: 0,
+            displayedText: '',
+            charIndex: 0,
+            fadingOut: false,
+            visibleStyle: {
+                opacity: 1,
+                transition: 'opacity 1s ease'
+            },
+            fadeOutStyle: {
+                opacity: 0,
+                transition: 'opacity 1s ease'
+            }
+        };
+    },
+    methods: {
+       
+        splitText() {
+            this.fragments = this.model.text.split('_');
+        },
+
+        displayNextFragment() {
+            const currentFragment = this.fragments[this.currentIndex];
+        
+            if (this.charIndex < currentFragment.length) {
+                this.displayedText += currentFragment[this.charIndex];
+                this.charIndex++;
+                setTimeout(this.displayNextFragment, 60);
+            } else {
+                setTimeout(() => {
+                    this.fadingOut = true; 
+                    setTimeout(this.showNextFragment, 1500); 
+                }, 500);
+            }
+        },
+        showNextFragment() {
+            
+            this.fadingOut = false;
+            this.displayedText = '';
+            this.charIndex = 0;
+
+           
+            this.currentIndex = (this.currentIndex + 1) % this.fragments.length;
+
+           
+            this.startTyping();
+        },
+
+        startTyping() {
+            this.displayNextFragment();
+        }
+    },
+    mounted() {
+        this.splitText(); 
+        this.startTyping(); 
+    }
+};
+
+app.component('topbarcustom_topbar', {
+    extends: topbarcustom_topbar,
+    template: '#topbarcustom_topbar'
+});
